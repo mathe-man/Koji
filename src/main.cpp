6e904836d4e6330c829435d2a831edb2a2a2a7f0
mathe-man <mathieumousset6@gmail.com>
2025-10-08 14:31:07 +0200

    Fix segfault caused by platform data

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <glfw3.h>
 #include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
 #include <iostream>
+
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <glfw3native.h>
+
+
+// define the screen size to 1920*1080 (HD) or 1280*720
+#ifdef HD_WINDOW_SIZE
+    constexpr uint32_t WINDOW_WIDTH = 1920;
+    constexpr uint32_t WINDOW_HEIGHT = 1080;
+#else
+    constexpr uint32_t WINDOW_WIDTH = 1280;
+    constexpr uint32_t WINDOW_HEIGHT = 720;
+#endif
 
 int main() {
     // Init GLFW
@@ -10,27 +24,45 @@ int main() {
     }
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // bgfx gère le rendu
-    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW + BGFX", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "GLFW + BGFX", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create window!\n";
         glfwTerminate();
         return -1;
     }
 
-    // Init bgfx
+    // setuo bgfx init settings
     bgfx::Init init;
-    init.type = bgfx::RendererType::Count; // auto-select renderer
-    init.resolution.width = 800;
-    init.resolution.height = 600;
+    init.type = bgfx::RendererType::Vulkan;
+    init.vendorId = BGFX_PCI_ID_NONE;
+    init.resolution.width = WINDOW_WIDTH;
+    init.resolution.height = WINDOW_HEIGHT;
     init.resolution.reset = BGFX_RESET_VSYNC;
-    bgfx::init(init);
+    init.resolution.format = bgfx::TextureFormat::BGRA8;
+
+    bgfx::PlatformData pd{};
+    pd.nwh = glfwGetWin32Window(window);
+    pd.ndt = nullptr;
+    pd.context = nullptr;
+    pd.backBuffer = nullptr;
+    pd.backBufferDS = nullptr;
+    init.platformData = pd; // Giving the platform data this way instead of using bgfx::PlatformData avoid the segfault
+
+
+    if (!bgfx::init(init)) {
+        std::cerr << "Failed to initialize bgfx!\n";
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+    }
+
 
     // Loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
         bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030ff);
-        bgfx::setViewRect(0, 0, 0, 800, 600);
+        bgfx::setViewRect(0, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
         bgfx::touch(0); // ensure view 0 is cleared
         bgfx::frame();
     }
