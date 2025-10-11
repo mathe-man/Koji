86138d1c3dd81826080e782b9bbe19f06db804d6
mathe-man <mathieumousset6@gmail.com>
2025-10-11 14:56:11 +0200

    Add initial value for Transform

--- include/Koji/ECS/components/Transform.h
+++ include/Koji/ECS/components/Transform.h
@@ -4,8 +4,8 @@
 namespace Koji {
 
 struct Transform {
-    glm::vec3 position;
-    glm::quat rotation;
+    glm::vec3 position{0.0F};
+    glm::quat rotation{};
     glm::vec3 scale{1.f, 1.f, 1.f};
 };
 
