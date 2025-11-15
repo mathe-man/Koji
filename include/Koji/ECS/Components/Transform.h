da41e62080adc8a8f76a4fec581d7c70f9464722
mathe-man <mathieumousset6@gmail.com>
2025-11-15 21:12:49 +0100

    Implemented PhysicsSystem

--- include/Koji/ECS/Components/Transform.h
+++ include/Koji/ECS/Components/Transform.h
@@ -2,16 +2,17 @@
 #include <glm/glm.hpp>
 #define GLM_ENABLE_EXPERIMENTAL
 #include "glm/gtx/transform.hpp"
+#include "glm/gtc/quaternion.hpp"
 
 namespace Koji::ECS
 {
     struct Transform {
         glm::vec3 position = glm::vec3(0.0f);
-        glm::vec3 rotation = glm::vec3(0.0f);
+        glm::quat rotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
         glm::vec3 scale = glm::vec3(1.0f);
     
-        glm::mat4 GetModelMatrix() const {
-            glm::mat4 model = glm::mat4(1.0f);
+        [[nodiscard]] glm::mat4 GetModelMatrix() const {
+            auto model = glm::mat4(1.0f);
             model = glm::translate(model, position);
             model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
             model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
---
+++ src/ECS/Systems/PhysicsSystem.cpp
@@ -0,0 +1,46 @@
+#include <Koji/ECS/Systems/PhysicsSystem.h>
+#include <Koji/ECS/>
+
+#include "Koji/ECS/Components/PhysicsBody.h"
+#include "Koji/ECS/Components/Transform.h"
+
+using namespace Koji::ECS;
+
+void PhysicsSystem::SyncTransformsToPhysics(entt::registry& ecs) {
+    auto view = ecs.view<PhysicsBody, Transform>();
+    
+    view.each(
+        [&](PhysicsBody& pb, Transform& tr) {
+
+            if (!pb.body) return;
+
+            // Convert glm → reactphysics3d structures
+            reactphysics3d::Vector3 pos(tr.position.x, tr.position.y, tr.position.z);
+            reactphysics3d::Quaternion rot(tr.rotation.x, tr.rotation.y,
+                                           tr.rotation.z, tr.rotation.w);
+
+            reactphysics3d::Transform t(pos, rot);
+
+            pb.body->setTransform(t);
+        }
+    );
+}
+
+
+void PhysicsSystem::SyncTransformsFromPhysics(entt::registry& ecs) {
+    auto view = ecs.view<PhysicsBody, Transform>();
+    
+    view.each<PhysicsBody, Transform>(
+        [&](PhysicsBody& pb, Transform& tr) {
+
+            if (!pb.body) return;
+
+            reactphysics3d::Transform t = pb.body->getTransform();
+            reactphysics3d::Vector3 pos = t.getPosition();
+            reactphysics3d::Quaternion rot = t.getOrientation();
+
+            tr.position = glm::vec3(pos.x, pos.y, pos.z);
+            tr.rotation = glm::quat(rot.w, rot.x, rot.y, rot.z);
+        }
+    );
+}
